55c6181bb96545e98ebe44c089a53c8d3d5fc9f0
tommy.tang <tommy.tang@lixinchuxing.com>
2020-12-23 17:40:08 +0800

    feat: 粒子特效

---
+++ vue.config.js
@@ -0,0 +1,6 @@
+const isProduction = process.env.RUN_ENV === 'production';
+const baseUrl = 'https://tomgou.github.io/geometry_particle/www';
+module.exports = {
+  baseUrl: baseUrl,
+  outputDir: 'www',
+}
